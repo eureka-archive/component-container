669ce53bd1ed39f2dc24673285a904d7d43e5f69
velkuns <romain.inscription@hotmail.fr>
2018-02-16 19:30:55 +0100

    Improve service parsing
     * Replace reference to another services
     * Fix phpdoc
     * Fix formating

--- src/Container.php
+++ src/Container.php
@@ -9,7 +9,6 @@
 
 namespace Eureka\Component\Container;
 
-use Eureka\Component\Container\Exception\NotFoundException;
 use Psr\Container\ContainerInterface;
 
 /**
@@ -53,7 +52,7 @@ class Container implements ContainerInterface
     public function get($id)
     {
         if (!$this->has($id)) {
-            throw new NotFoundException('No instance for given key! (key: ' . $id . ')');
+            throw new Exception\NotFoundException('No instance for given key! (key: ' . $id . ')');
         }
 
         return $this->instances[$id];
@@ -73,8 +72,8 @@ class Container implements ContainerInterface
      *
      * @param  string $id Key name to retrieve the instance
      * @param  object $instance Instance to attach
-     * @return self
-     * @throws \LogicException
+     * @return $this
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function attach($id, $instance)
     {
@@ -92,7 +91,8 @@ class Container implements ContainerInterface
      * Implicit destruct the instance.
      *
      * @param  string $id Key name to retrieve the instance
-     * @return self
+     * @return $this
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function detach($id)
     {
@@ -107,7 +107,8 @@ class Container implements ContainerInterface
      * Initialize container from data array.
      *
      * @param  array $array
-     * @return self
+     * @return $this
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function initFromArray(array $array)
     {
@@ -117,4 +118,80 @@ class Container implements ContainerInterface
 
         return $this;
     }
+
+    /**
+     * Create new container & init it from an array.
+     * @param  array $array
+     * @return Container
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    public static function makeFromArray(array $array, $repass = 5)
+    {
+        $retry     = [];
+        $container = new self();
+
+        for ($pass = 0; $pass < $repass; $pass++) {
+            if ($pass === 0) {
+                $services = $array;
+            } else {
+                $services = $retry;
+                $retry    = [];
+            }
+
+            foreach ($services as $name => $service) {
+
+                if (!isset($service['params']) || !is_array($service['params'])) {
+                    $container->attach($name, new $service['class']());
+                    continue;
+                }
+
+                //~ When have incomplete replace, service replacement will be retried.
+                $params = $service['params'];
+                if (!self::replace($container, $params)) {
+                    $retry[$name] = $service;
+                    continue;
+                }
+
+                $container->attach($name, new $service['class'](...$params));
+            }
+        }
+
+        if (!empty($retry)) {
+            throw new \Exception('Existing non-replaced services after ' . $repass . ' re-pass!');
+        }
+
+        return $container;
+    }
+
+    /**
+     * @param  array|string|int|bool $params
+     * @return bool
+     */
+    private static function replace(Container $container, &$params)
+    {
+        $isComplete = true;
+
+        foreach ($params as $index => &$param) {
+            //~ Handle array
+            if (is_array($param)) {
+                $isComplete = ($isComplete && self::replace($container, $param));
+                continue;
+            }
+
+            if (!is_string($param) || 0 !== strpos($param, '@')) {
+                continue;
+            }
+
+            //~ Handle services param
+            $subservice = substr($param, 1);
+            if ($container->has($subservice)) {
+                $param = $container->get($subservice);
+            } else {
+                $isComplete = false;
+            }
+        }
+
+        return $isComplete;
+    }
 }
+
